c84fb87ffca13b8d1658a53675da874b96946ea3
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-04-14 14:31:47 -0700

    Updated WAR
    Everything works. fixed bugs. Next is adding shuffle

--- All API's/card game/js/main.js
+++ All API's/card game/js/main.js
@@ -13,8 +13,23 @@ fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
 
 document.querySelector('button').addEventListener('click', drawTwo)
 
+let lastCallDrawTwo = true
+
 function drawTwo(){
 
+    
+    if(lastCallDrawTwo === false){
+        document.querySelector('#card2').classList.toggle('reset')
+        document.querySelector('#card3').classList.toggle('reset')
+        document.querySelector('#card4').classList.toggle('reset')
+        document.querySelector('#card5').classList.toggle('reset')
+        document.querySelector('#card6').classList.toggle('reset')
+        document.querySelector('#card7').classList.toggle('reset')
+        document.querySelector('#card8').classList.toggle('reset')
+        document.querySelector('#card9').classList.toggle('reset')
+        lastCallDrawTwo = true
+    }
+    
   const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
 
 
@@ -34,11 +49,8 @@ function drawTwo(){
             document.querySelector('h3').innerText = 'Time for War!'
             document.querySelector('button').removeEventListener('click', drawTwo)
             document.querySelector('button').addEventListener('click', warTime)
-        }
-        let tableRef = document.querySelector('.reset')
-        for(let i = 0; i <= cards.images.length; i++)
-        {
-            tableRef.delete(i)
+            document.querySelector('button').innerText = 'TO WAR!!'
+            //document.querySelector('#nikki').classList.toggle('hidden')
         }
       })
       .catch(err => {
@@ -62,34 +74,52 @@ function convertToNum(val){
 
 function warTime(){
 
+    if(lastCallDrawTwo === true){
+        document.querySelector('#card2').classList.toggle('reset')
+        document.querySelector('#card3').classList.toggle('reset')
+        document.querySelector('#card4').classList.toggle('reset')
+        document.querySelector('#card5').classList.toggle('reset')
+        document.querySelector('#card6').classList.toggle('reset')
+        document.querySelector('#card7').classList.toggle('reset')
+        document.querySelector('#card8').classList.toggle('reset')
+        document.querySelector('#card9').classList.toggle('reset')
+        lastCallDrawTwo = false
+    }else{
+        document.querySelector('#card0').src = document.querySelector('#card8').src
+        document.querySelector('#card1').src = document.querySelector('#card9').src
+    }
+
 fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=8`)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
         console.log(data)
-        document.querySelector('#card0').src = data.cards[0].image
-        document.querySelector('#card1').src = data.cards[1].image
-        document.querySelector('#card2').src = data.cards[2].image
-        document.querySelector('#card3').src = data.cards[3].image
-        document.querySelector('#card4').src = data.cards[4].image
-        document.querySelector('#card5').src = data.cards[5].image
-        document.querySelector('#card6').src = data.cards[6].image
-        document.querySelector('#card7').src = data.cards[7].image
+        document.querySelector('#card2').src = data.cards[0].image
+        document.querySelector('#card3').src = data.cards[1].image
+        document.querySelector('#card4').src = data.cards[2].image
+        document.querySelector('#card5').src = data.cards[3].image
+        document.querySelector('#card6').src = data.cards[4].image
+        document.querySelector('#card7').src = data.cards[5].image
+        document.querySelector('#card8').src = data.cards[6].image
+        document.querySelector('#card9').src = data.cards[7].image
         let player1Val = convertToNum(data.cards[6].value)
         let player2Val = convertToNum(data.cards[7].value)
         if(player1Val > player2Val){
             document.querySelector('h3').innerText = 'Player 1 Wins'
             document.querySelector('button').removeEventListener('click', warTime)
             document.querySelector('button').addEventListener('click', drawTwo)
+            document.querySelector('button').innerText = 'Deal Two Cards!'
         }else if(player1Val < player2Val){
             document.querySelector('h3').innerText = 'Player 2 Wins'
             document.querySelector('button').removeEventListener('click', warTime)
             document.querySelector('button').addEventListener('click', drawTwo)
+            document.querySelector('button').innerText = 'Deal Two Cards!'
         }else{
             document.querySelector('h3').innerText = 'Time for War AGAIN!'
         }
     })
     .catch(err => {
         console.log(`error ${err}`)
+        document.querySelector('h3').innerText = 'Error no cards'
     });
 }
 
